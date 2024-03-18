c6b8cbe0af23b3dc3ad03c529dd68ec8a968c943
ahmed <ahmed.usman@ovrlod.com>
2024-03-18 15:07:07 +0500

    Server side Event handling.

---
+++ Models/Blocks/CustomFormContainerBlock.cs
@@ -0,0 +1,18 @@
+﻿using EPiServer.Forms.Core;
+using EPiServer.Forms.Implementation.Elements;
+using EPiServer.ServiceLocation;
+
+namespace Optimizely_Project.Models.Blocks
+{
+    [ContentType(DisplayName = "Custom Standard Container",
+    GUID = "c24ead35-a13d-478a-ac15-941e74bd7bc0",
+    Description = "Custom form Episerver.",
+    GroupName = EPiServer.Forms.Constants.FormElementGroup_Container)]
+    [ServiceConfiguration(typeof(IFormContainerBlock))]
+
+    [ImageUrl("~/resources/icons/custom-form-block.jpg")]
+    public class CustomFormContainerBlock : FormContainerBlock
+    {
+        public virtual string CustomName { get;set; }
+    }
+}
